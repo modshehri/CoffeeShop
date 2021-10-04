c33ae3dacbbadf645455d31c4fe9ea981f05e071
modshehri <mod.shehri@hotmail.com>
2021-10-04 21:32:12 +0300

    Finalizing the postman collection

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -104,19 +104,15 @@ def verify_decode_jwt(token):
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     jwks = json.loads(jsonurl.read())
     unverified_header = jwt.get_unverified_header(token)
-    print(token)
-    print(unverified_header)
-    rsa_key = {}
+
     if 'kid' not in unverified_header:
         raise AuthError({
             'code': 'invalid_header',
             'description': 'Authorization malformed.'
         }, 401)
 
-    print(unverified_header['kid'])
 
     for key in jwks['keys']:
-        print(key['kid'])
         if key['kid'] == unverified_header['kid']:
             rsa_key = {
                 'kty': key['kty'],
